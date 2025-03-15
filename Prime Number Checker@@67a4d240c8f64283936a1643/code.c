4e22e66a856a7845372a1c655d573065c291a499
adityagarg27 <aditya0093.becse24@chitkara.edu.in>
2025-03-15 09:45:20 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -18,15 +18,41 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+
+// int isPrime(int num) {
+    // if (num <= 1) return 0; // 0 and 1 are NOT prime
+    // if (num == 2) return 1; // 2 is the ONLY even prime number
+    // if (num % 2 == 0) return 0; // Exclude other even numbers
+
+    // Check divisibility from 3 to sqrt(num), skipping even numbers
+    // for (int i = 3; i * i <= num; i += 2) { 
+        // if (num % i == 0) return 0; // If divisible, not prime
+    // }
+    // return 1; // If no divisors found, it's prime
+// }
+
+// int main() {
+//     int t;
+//     scanf("%d", &t); // Input number of test cases
+
+//     while (t--) {
+//         int num;
+//         scanf("%d", &num);
+//         printf("%d\n", isPrime(num)); // Print 1 if prime, 0 otherwise
+//     }
+//     return 0;
+// }
 #include <stdio.h>
 
+// Function to check if a number is prime
 int isPrime(int num) {
     if (num <= 1) return 0; // 0 and 1 are NOT prime
     if (num == 2) return 1; // 2 is the ONLY even prime number
     if (num % 2 == 0) return 0; // Exclude other even numbers
 
-    // Check divisibility from 3 to sqrt(num), skipping even numbers
-    for (int i = 3; i * i <= num; i += 2) { 
+    int i; // Declare variable before loop (for C90 compatibility)
+    for (i = 3; i * i <= num; i += 2) { // Check only odd numbers
         if (num % i == 0) return 0; // If divisible, not prime
     }
     return 1; // If no divisors found, it's prime
@@ -34,12 +60,17 @@ int isPrime(int num) {
 
 int main() {
     int t;
-    scanf("%d", &t); // Input number of test cases
+    
+    // Ensure valid input for test cases
+    if (scanf("%d", &t) != 1) return 1; 
 
-    while (t--) {
+    while (t--) { // Loop for multiple test cases
         int num;
+        while (getchar() != '\n'); // Clear input buffer if needed
         scanf("%d", &num);
         printf("%d\n", isPrime(num)); // Print 1 if prime, 0 otherwise
     }
-    return 0;
+
+    return 0; // Explicit return statement
 }
+
