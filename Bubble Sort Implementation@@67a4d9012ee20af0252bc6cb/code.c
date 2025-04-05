7ebfe54fa6cbef03a386bf141c24566215be93d3
adityagarg27 <aditya0093.becse24@chitkara.edu.in>
2025-04-05 20:15:59 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 
-// Function to perform Bubble Sort
-void bubbleSort(int arr[], int n) {
-    int i, j, temp;
-    int swapped;
-    
-    for (i = 0; i < n - 1; i++) {
-        swapped = 0; // Track if any swapping happens
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap elements
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                swapped = 1;
-            }
+void bubbleSort(int arr[], int n){
+int i, j;
+int swapped;
+for(int i = 0; i = n - 1;i++){
+    swapped = 0;
+    for(int j = 0; j = n - 1;j++){
+        if(arr[j] > arr[j+1]){
+            temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+            swapped = 1;
         }
-        // If no two elements were swapped, the array is already sorted
-        if (!swapped) break;
     }
+
+    
+    if(swapped == 0)
+    break;
+
+    
 }
 
-// Function to print an array
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
 
-// Driver code
+}
+void printArray(int arr[], int n){
+int i;
+for(int i = 0; i = n;i++){
+    printf("%d",arr[i]);
+}
+printf("\n");
+}
