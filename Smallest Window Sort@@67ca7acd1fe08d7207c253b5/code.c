046793775364ffc660baf970d74e12257d1962b7
adityagarg27 <aditya0093.becse24@chitkara.edu.in>
2025-04-06 12:32:10 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-// Function to check if the array is sorted
-int isSorted(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        if (arr[i] > arr[i + 1]) { // If any element is greater than the next one, array is not sorted
-            return 0;
-        }
-    }
-    return 1;
+int findUnsortedSubarray(int arr[],int n){
+    int start = 0, end = n - 1;
+    while (start < n - 1 && arr[start] <= arr[start+1])
+    start++;
+
+if (start == n - 1)
+returbn 0;
+
+while(end > 0 && arr[end] >=arr[end - 1])
+end--;
+
+int min = arr[start], max = arr[start];
+for(int i = start; i <= end; i++){
+    if(arr[i] < min)
+    min = arr[i];
+    if (arr[i] > max)
+    max = arr[i];
 }
+while (start > 0 && arr[start - 1] > min)
+start--;
+while (end < n - 1 && arr[end + 1] < max)
+end++;
+return end - start + 1;
 
-// Comparison function for sorting
-int compare(const void *a, const void *b) {
-    return (*(int*)a - *(int*)b); // Ascending order sorting
+
 }
-
-
