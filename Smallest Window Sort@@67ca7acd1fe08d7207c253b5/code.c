5a35c6bbacb0c5a3ec529f7098d2d94d617ffe24
adityagarg27 <aditya0093.becse24@chitkara.edu.in>
2025-04-06 12:49:36 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
-
-int findUnsortedSubarray(int arr[],int n){
-    int start = 0, end = n - 1;
-    while (start < n - 1 && arr[start] <= arr[start+1])
-    start++;
-
-if (start == n - 1)
+int findUnsortedSubarray(int arr[], int n){
+int start = 0, int end = n - 1;
+while( start < n - 1 && arr[start] <= arr[start+1])
+start++;
+if(start == 1)
 return 0;
-
-while(end > 0 && arr[end] >=arr[end - 1])
-end--;
-
+while( end  > 0 && arr[end] >= arr[end-1])
+end--
 int min = arr[start], max = arr[start];
-for(int i = start; i <= end; i++){
-    if(arr[i] < min)
-    min = arr[i];
-    if (arr[i] > max)
-    max = arr[i];
+for (int i = start; i <= end; i++){
+if(arr[i] < min)
+min = arr[i];
+if(arr[i] > max)
+max = arr[i];
 }
-while (start > 0 && arr[start - 1] > min)
+while(start > 0 && arr[start-1] > min)
 start--;
-while (end < n - 1 && arr[end + 1] < max)
+while(end < n - 1 && arr[end + 1] < max)
 end++;
 return end - start + 1;
-
-
 }
+
+
