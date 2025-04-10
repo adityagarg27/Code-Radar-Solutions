8e8925eeb147e5c28672da38d38ea3d7478d8531
adityagarg27 <aditya0093.becse24@chitkara.edu.in>
2025-04-10 23:40:47 +0530

    Solution to Kth Missing Positive Number

--- Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
+++ Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
-void bubbleSort(int arr[], int n){
-    int temp;
-    for(int i = 0; i < n - 1;i++){
-        for(int j = 0; j < n - i - 1;j++){
-            if(arr[j] > arr[j + 1]){
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+int fimdKthMissing(int arr[], int n, int k){
+    int left = 0; int right = n - 1;
+    while(left <= right){
+        int mid = left + (right - left)/2;
+        int missing = arr[mid] - (mid + 1);
+        if(missing < k)
+        left = mid + 1;
+        else
+        right = mid - 1;
     }
-}
-int kthSmallest(int arr[], int n, int k){
-    bubbleSort(arr,n);
-    if (k >= 1 && k <= n){
-        return arr[k - 1];
-    }
-    else {
-        return -1;
-    }
-
+    return left + k;
 }
