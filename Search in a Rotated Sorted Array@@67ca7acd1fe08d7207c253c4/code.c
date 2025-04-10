c8bbd9a1f5e0cd3b1d1e76f3aa1bf46a0a376e5e
adityagarg27 <aditya0093.becse24@chitkara.edu.in>
2025-04-10 23:14:07 +0530

    Solution to Search in a Rotated Sorted Array

--- Search in a Rotated Sorted Array@@67ca7acd1fe08d7207c253c4/code.c
+++ Search in a Rotated Sorted Array@@67ca7acd1fe08d7207c253c4/code.c
@@ -1,22 +1,51 @@
+// #include <stdio.h>
+// int searchInRotatedArray(int n, int target, int arr[]){
+//     int left = 0; int right = n - 1;
+//     while (left <= right){
+//         int mid = left +(right - left)/2;
+//         if(arr[mid] == target)
+//         return mid;
+//         if (arr[left] <= arr[mid]){
+//             if(target >= arr[left] && target < arr[mid])
+//             right = mid - 1;
+//             else
+//             left = mid + 1;
+//         }
+//         else{
+//             if(target >= arr[mid] && target <= arr[right])
+//             left = mid + 1;
+//             else
+//             right = mid - 1;
+//         }
+//     }
+//     return -1;
+// }
 #include <stdio.h>
-int searchInRotatedArray(int n, int target, int arr[]){
-    int left = 0; int right = n - 1;
-    while (left <= right){
-        int mid = left +(right - left)/2;
-        if(arr[mid] == target)
-        return mid;
-        if (arr[left] <= arr[mid]){
-            if(target >= arr[left] && target < arr[mid])
-            right = mid - 1;
+
+int searchInRotatedArray(int arr[], int n, int target) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+
+        // Left half is sorted
+        if (arr[low] <= arr[mid]) {
+            if (target >= arr[low] && target < arr[mid])
+                high = mid - 1;
             else
-            left = mid + 1;
+                low = mid + 1;
         }
-        else{
-            if(target >= arr[mid] && target <= arr[right])
-            left = mid + 1;
+        // Right half is sorted
+        else {
+            if (target > arr[mid] && target <= arr[high])
+                low = mid + 1;
             else
-            right = mid - 1;
+                high = mid - 1;
         }
     }
-    return -1;
+
+    return -1; // Not found
 }
